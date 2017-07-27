1e3ad813d26b7942f498a2501eed0f2aac1b3fbc
Makoto Ito <makoto@unity3d.com>
2017-07-27 12:54:34 +0900

    First commit

---
+++ Assets/Editor/MaterialCombine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class MaterialCombine {
+
+
+    [MenuItem("Assets/Combine same materials")]
+    static public void CombineMaterial()
+    {
+        Renderer[] renderers = Selection.activeGameObject.GetComponentsInChildren<Renderer>();
+        Dictionary<Color, Material> mats = new Dictionary<Color, Material>();
+        foreach( Renderer renderer in renderers )
+        {
+            if (renderer == null) continue;
+            Material mat = renderer.sharedMaterial;
+            if (mat.mainTexture != null) continue;
+            Color col  = mat.color;
+            if ( mats.ContainsKey(col))
+            {
+                renderer.sharedMaterial = mats[col];
+            }
+            else
+            {
+                mats[col] = mat;
+            }
+        }
+    }
+}
